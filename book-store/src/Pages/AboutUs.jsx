37aed9101f6d78944466772237892ef64d7b1d6c
Fakorede Peter Okikiola <holuwaconquer488@gmail.com>
2026-05-15 09:27:33 +0100

    updated the icon in connect with us

--- book-store/src/Pages/AboutUs.jsx
+++ book-store/src/Pages/AboutUs.jsx
@@ -411,23 +411,28 @@ const AboutUs = () => {
             
             <div className="flex justify-center gap-6 mb-12">
               <a 
-                href="https://instagram.com/estarrbookart.hubafrika" 
+                href="https://www.instagram.com/theestheradesina" 
                 target="_blank" 
                 rel="noopener noreferrer"
                 className="p-4 bg-white/20 rounded-full hover:bg-white/30 transition-all hover:scale-110"
               >
-                {/* <HiOutlineInstagram className="w-8 h-8" /> */}
-                insta
+                <FaInstagram className="w-8 h-8" />
               </a>
               <a 
-                href="https://threads.net/@i_am_jaszi" 
+                href="https://www.facebook.com/estheribukunoluwa.adesina.5" 
                 target="_blank" 
                 rel="noopener noreferrer"
                 className="p-4 bg-white/20 rounded-full hover:bg-white/30 transition-all hover:scale-110"
               >
-                <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 24 24">
-                  <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 4c1.1 0 2 .9 2 2s-.9 2-2 2-2-.9-2-2 .9-2 2-2zm0 13c-2.33 0-4.31-1.46-5.11-3.5h10.22c-.8 2.04-2.78 3.5-5.11 3.5z"/>
-                </svg>
+                <FaFacebook className="w-8 h-8" />
+              </a>
+              <a 
+                href="https://www.linkedin.com/in/esther-adesina-9a4641242/" 
+                target="_blank" 
+                rel="noopener noreferrer"
+                className="p-4 bg-white/20 rounded-full hover:bg-white/30 transition-all hover:scale-110"
+              >
+                <FaLinkedin className="w-8 h-8" />
               </a>
             </div>
 
